0ae94b9cb36f43eee52ddf0e9d794e8cd7439bea
Richard Lopez <richardsamuellopez@gmail.com>
2019-10-09 23:43:59 -0400

    update contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,7 @@ export default class ContactPage extends React.Component {
     email: "",
     message: "",
   }
+
   handleInputChange = event => {
     const target = event.target
     const value = target.value
@@ -15,48 +16,44 @@ export default class ContactPage extends React.Component {
       [name]: value,
     })
   }
-  handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.name} ${this.state.email} ${this.state.message}!`)
-  }
+
   render() {
     return (
       <Layout>
         <div>
           <h1>Contact Us</h1>
-        {this.state.name}{this.state.email}{this.state.message}
-      <form method="post" netlify-honeypot="bot-field" data-netlify="true" onSubmit={this.handleSubmit}>
-        <input type="hidden" name="bot-field" />
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="text"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Message
-          <input
-            type="textarea"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      </div>
+          <form name="ContactUs" method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="bot-field" />
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Email
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Message
+              <input
+                type="textarea"
+                name="message"
+                value={this.state.message}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </Layout>
     )
   }
