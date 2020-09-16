f05ec44d58ef30ed78abd1f5fff2a19bc5464233
Richard Lopez <richardsamuellopez@gmail.com>
2020-09-15 20:25:10 -0400

    Update guid and candidate views

--- src/pages/candidate-views.js
+++ src/pages/candidate-views.js
@@ -8,15 +8,24 @@ const CandidatePage = () => {
  return (
   <Layout>
     <SEO title="Candidate Views Questionaire & Answers" />
-    <h1>Candidate Views Questionaire & Answers</h1>
-    <div className="flex-container">
-      <div className="item">
-        <p>This will be the landing page for the Candidate Views QUestionaire & Answers page.</p>
-      </div>
-      <div className="item">
-        <ul>
-        </ul>
-      </div>
+    <h1 className="uppercase">Candidate Views</h1>
+    <div>
+      <p>
+        There are many reasons to vote for a candidate: party affiliation, position on taxes, jobs and national security.  Whether you’re Republican, Democrat or Independent voting for local, state or federal candidates, we’d like you to consider the candidate’s views on energy, environment and climate when making your decision.
+      </p>
+      <p>
+        Candidates in select races were invited to share their views on energy, environment and climate.  Candidates were contacted multiple times on several occasions; refusing to respond is indicative of having an opposing view.
+      </p>
+      <p>
+        In addition, we also review the candidate’s policy positions, voting record and campaign contributions.
+        Due to limited resources, only major party candidates in select races are evaluated.
+      </p>
+      <h3 className="uppercase">FIND YOUR LEGISLATORS HERE -</h3>
+      <p>
+        <a href="http://iga.in.gov/legislative/find-legislators/" target="_blank" rel="noopener noreferrer">http://iga.in.gov/legislative/find-legislators/</a>
+      </p>
+      <h3 className="uppercase">FIND CANDIDATE VIEWS HERE -</h3>
+      <p>The candidate view will be available in October.</p>
     </div>
   </Layout>
  )
--- src/pages/guide.js
+++ src/pages/guide.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DocList from "../components/docList"
-
+import { Link } from "gatsby"
 import './guide.css'
 
 const GuidePage = () => {
@@ -12,13 +12,20 @@ const GuidePage = () => {
     <h1>Green Voters Guide</h1>
     <div className="flex-container">
       <div className="item">
-        <p>There are many reasons to vote for a candidate: party affiliation, position on taxes, jobs and national security.  Whether you’re Republican,  Democrat or Independent voting for local, state or federal candidates, we’d like you to consider the candidate’s views on energy, environment and climate when making your decision.  We need elected officials who understand the ongoing energy and climate challenges we face today to successfully lead us through these changes to build sustainable and resilient communities.</p>
-        <p>The Green Voters Guide represents our best assessment of policies, positions, statements and votes to identify the "green" candidate(s) within a race. These are candidates who we believe are most likely to address climate issues. The more environmentally-friendly candidate(s) in our opinion get a green checkmark. Due to limited resources, only major party candidates in select races are evaluated.</p>
+        <p>
+          The 2020 election comes at a pivotal time in history when climate science warns of critical thresholds beyond which damage is irreversible.  Like pandemic preparedness, elected officials who understand the ongoing energy and climate challenges are best qualified to lead us through these changes.
+        </p>
+        <p>
+          The Green Voters Guide represents a non-partisan evaluation of policies, positions, statements and votes to identify the "greener" candidate(s) within select races.  The environmentally-friendly candidate(s) who in our opinion are most likely to address climate issues get a green checkmark. Due to limited resources, only major party candidates in select races are evaluated.
+        </p>
       </div>
       <div className="item">
         <ul>
           <DocList search="2020" />
-          <DocList search="Solar" />
+          <li>
+            <Link className="uppercase" to="/candidate-views">Candidate Views</Link>
+            </li>
+          {/* <DocList search="Solar" /> */}
         </ul>
       </div>
     </div>
