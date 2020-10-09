555ebc38a91c5f6343486b8fcce80e3e05c18b4e
Richard Lopez <richardsamuellopez@gmail.com>
2020-10-08 23:15:22 -0400

    Update text

--- src/pages/candidate-views.js
+++ src/pages/candidate-views.js
@@ -13,22 +13,38 @@ const CandidatePage = () => {
     <div className="flex-container">
      <div className="item">
       <p>
-        There are many reasons to vote for a candidate: party affiliation, position on taxes, jobs and national security.  Whether you’re Republican, Democrat or Independent voting for local, state or federal candidates, we’d like you to consider the candidate’s views on energy, environment and climate when making your decision.
+        There are many reasons to vote for a candidate: party affiliation, position on taxes, jobs and national security.  Whether you’re Republican, Democrat or Independent, we’d like you to consider the candidate’s views on energy, environment and climate when making your decision.
       </p>
       <p>
-        Candidates in select races were invited to share their views on energy, environment and climate.  Candidates were contacted multiple times on several occasions; refusing to respond is indicative of having an opposing view.
+        VGI volunteers put together non-partisan resources to help inform voters. Where available, it includes a summary of the candidate’s policy positions, voting record, ratings and a listing of campaign contributions from the fossil fuel industry based on publicly available information.
       </p>
       <p>
-        In addition, we also review the candidate’s policy positions, voting record and campaign contributions.
-        Due to limited resources, only major party candidates in select races are evaluated.
+        In addition, candidates in select races were invited to share their views through a questionnaire. Their responses (if any) are provided. Candidates were contacted multiple times; lack of a response to our inquiries is noted below. These candidates have failed to provide voters with their positions on the key issue of our global environment.
       </p>
-        <h4 className="underline">Election Information</h4>
-
-        <p><span className="bold italic">Voter Registration</span> in Indiana ends on October 5, 2020.</p>
-
-        <p><span className="bold italic">Early Voting</span> dates and polling places are available from your county Election Office.</p>
-
-        <p>For <span className="bold italic">Indiana Absentee Ballot</span> instructions, go to:  <a href="http://www.in.gov/sos/elections/2402.htm">www.in.gov/sos/elections/2402.htm</a>.  Your county Elections Office sets the deadline for <span className="bold italic">Absentee Ballot Applications</span> (in October). Mail-in absentee ballots must be <span className="italic">received</span> by 12 noon, Tuesday November 3, so be certain to mail them in early.</p>
+      <p>
+        Due to limited volunteer time and resources, only major party candidates in select contested races were evaluated.
+      </p>
+      <h4 className="underline">Election Information</h4>
+      <ul>
+        <li>
+          Voter registration ends October 5.
+        </li>
+        <li>
+          Early voting begins October 6.  Find location & hours <a href="https://indianavoters.in.gov/" target="_blank">here</a> or at your local county Election Office.
+        </li>
+        <li>
+          Applications to Vote By Mail are due October 22, 11:59 pm (local time). Apply online <a href="https://indianavoters.in.gov/" target="_blank">here</a>.
+        </li>
+        <li>
+          Mail-in absentee ballots must be received by 12 noon (local time), November 3, so be certain to mail in early. Absentee ballots can also be dropped off at your county Election Office during office hours.  More info here.
+        </li>
+        <li>
+          Election Day is November 3, 6 am – 6 pm.
+        </li>
+        <li>
+          To see who’s on your ballot, go to <a href="https://indianavoters.in.gov/" target="_blank">https://indianavoters.in.gov/</a>.  Then to get into your Voter Portal, click on either “CHECK VOTING STATUS” or “VOTING LOCATION”.
+        </li>
+      </ul>
       <p>
         <span style={{fontSize:'1.5em'}}className="uppercase bold">FIND YOUR LEGISLATORS HERE - </span>
         <a href="http://iga.in.gov/legislative/find-legislators/" target="_blank" rel="noopener noreferrer">http://iga.in.gov/legislative/find-legislators/</a>
@@ -41,6 +57,7 @@ const CandidatePage = () => {
       <div className="item">
         <ul>
           <DocList search="Candidate Views" />
+          <DocList search="Campaign" />
         </ul>
       </div>
     </div>
