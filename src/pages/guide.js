40be909d603a28340925521c50eedad7441f851c
Richard Lopez <richardsamuellopez@gmail.com>
2022-04-04 20:03:17 -0400

    Update text and remove documents

--- src/pages/guide.js
+++ src/pages/guide.js
@@ -13,7 +13,7 @@ const GuidePage = () => {
     <div className="flex-container">
       <div className="item">
         <p>
-          The 2020 election comes at a pivotal time in history when climate science warns of critical thresholds beyond which damage is irreversible.  Like pandemic preparedness, elected officials who understand the ongoing energy and climate challenges are best qualified to lead us through these changes.
+          This election comes at a pivotal time in history when climate science warns of critical thresholds beyond which damage is irreversible.  Like pandemic preparedness, elected officials who understand the ongoing energy and climate challenges are best qualified to lead us through these changes.
         </p>
         <p>
           The Green Voters Guide represents a non-partisan evaluation of policies, positions, statements and votes to identify the "greener" candidate(s) within select races.  The environmentally-friendly candidate(s) who in our opinion are most likely to address climate issues get a green checkmark. Due to limited resources, only major party candidates in select races are evaluated.
@@ -26,13 +26,6 @@ const GuidePage = () => {
           <DocList search="Green Voters Guide" />
           <DocList search="Solar" />
           {/* <DocList search="Press" /> */}
-        </ul>
-        <h5>Top Recipients of Campaign Funds from Coal & Utility Industries</h5>
-        <ul>
-          <DocList search="Soliday" />
-          <DocList search="Huston" />
-          <DocList search="Charbonneau" />
-          <DocList search="Koch" />
         </ul>
       </div>
     </div>
