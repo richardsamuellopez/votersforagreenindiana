909db787ed3d11118fbeeed09ee66411c321b113
Richard Lopez <richardsamuellopez@gmail.com>
2024-04-09 15:11:49 -0400

    Embed the guide and views from the google docs

--- src/pages/candidate-views.js
+++ src/pages/candidate-views.js
@@ -29,36 +29,8 @@ const CandidatePage = () => {
  return (
   <Layout>
     <SEO title="Candidate Views Questionaire & Answers" />
-    <div className="flex-container">
-      <div className="item">
-        <h1 className="uppercase">Candidate Views</h1>
-          <p>
-            There are many reasons to vote for a candidate: party affiliation, position on taxes, jobs and national security. Here, you’ll get a review of the candidate’s views on clean energy, clean air, clean water, climate and sustainability!  Where available, it includes the candidate’s policy positions, voting record, ratings and a listing of campaign contributions from the fossil fuel industry based on publicly available information.
-          </p>
-          <p>
-            In addition, candidates in select races are invited to share their views through a questionnaire. Their responses (if any) are provided. Candidates are contacted several times; lack of a response to our inquiries is noted. Failure to provide voters with their positions indicates their opposition or low priority.
-          </p>
-          <p>
-            Due to limited volunteer time and resources, only major party candidates in select contested races are evaluated.
-          </p>
-          <h4 className="underline">2024 Primary Election is on May 7, 6 am – 6 pm</h4>
-          <ul>
-            <li>
-              Early voting begins April 9th.  Find location & hours at your local County Election Office.
-            </li>
-            <li>
-              Register to Vote or Check Voting Status at the <a href="https://indianavoters.in.gov/">Indiana Voter Portal</a>.  Check Voting Status will also show who’s on your ballot and voting locations
-            </li>
-          </ul>
-          {/* <CandidateList data={data.allCandidates2022Csv} /> */}
-        </div>
-        <div className="item">
-          <ul>
-            {/* <li><Link to={'/candidate-views/2023GeneralCandidateViews/'}>Candidate Views for the 2023 General Election</Link></li> */}
-            {/* <li><Link to={'/candidate-views/PrimaryElection/'}>Candidate Views for the 2023 General Election</Link></li> */}
-          </ul>
-        </div>
-      </div>
+    <h1 className="uppercase">Candidate Views</h1>
+    <iframe style={{width: "100vw", height: "100vh", border: "", margin: '0 -20px'}} src="https://docs.google.com/document/d/e/2PACX-1vRQY2GI0YARp-QrYzRFgFEky7MDvKCzN1SUBsnQzDEDGhMCfxujznEE-Z6gRX4Vkg/pub?embedded=true"></iframe>
   </Layout>
  )
 }
--- src/pages/guide.js
+++ src/pages/guide.js
@@ -10,23 +10,8 @@ const GuidePage = () => {
  return (
   <Layout>
     <SEO title="Green voters guide" />
-    <h1>Green Voters Guide</h1>
-    <div className="flex-container">
-      <div className="item">
-        <p>
-          This election comes at a pivotal time in history when climate science warns of critical thresholds beyond which damage is irreversible.  Like pandemic preparedness, elected officials who understand the ongoing energy and climate challenges are best qualified to lead us through these changes.
-        </p>
-        <p>
-          The Green Voters Guide represents a non-partisan evaluation of policies, positions, statements and votes to identify the "greener" candidate(s) within select races.  The environmentally-friendly candidate(s) who in our opinion are most likely to address climate issues get a green checkmark. Due to limited resources, only major party candidates in select races are evaluated.
-        </p>
-      </div>
-      <div className="item">
-        <ul>
-          <DocList search="Solar" />
-          {/* <Link to={'/guide/2023GeneralGreenVotersGuide/'}><GVGButton /></Link> */}
-        </ul>
-      </div>
-    </div>
+    <h1 className="uppercase">Green Voters Guidee</h1>
+    <iframe style={{width: "100vw", height: "100vh", border: "none", margin: "0 -20px"}} src="https://docs.google.com/document/d/e/2PACX-1vTuQUOa_kaXVUWqXCxj_FcbRQsNSLTAbNnipx3BtmCQt70IX_7C-kExK3HJKb9JTQ/pub?embedded=true"></iframe>
   </Layout>
  )
 }
